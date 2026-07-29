41345c5f333478be5ae91c7502d35d2a2e95c3ea
A7reus <an1ndya@proton.me>
2026-07-29 20:17:24 +0600

    chore: initial project scaffold

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Docker deployed via GitHub Actions and Tailscale!\n");
+});
+server.listen(8000, "0.0.0.0", () =>
+  console.log("Server running on port 8000"),
+);
