10760e6944cb2cc0f700f9719507e5cfcfa1ab26
Anindya <an1ndya@proton.me>
2026-08-14 20:37:31 +0600

    docs(api): Create api docs and refactoring CREATE (#17)

--- server/src/controllers/postController.ts
+++ server/src/controllers/postController.ts
@@ -5,16 +5,33 @@ import { PostModel } from "../models/postModel.js";
 export const createPost = async (c: Context) => {
   try {
     const { title, content, author } = await c.req.json();
+    const cleanTitle = title.trim();
+    const cleanContent = content.trim();
+    const cleanAuthor = author.trim();
 
-    if (!title || !content || !author) {
-      return c.json({ error: "Title, content, and author are required" }, 400);
+    if (!cleanTitle) {
+      return c.json({ errCode: 100, errMsg: "Title is required" }, 400);
+    } else if (!cleanContent) {
+      return c.json({ errCode: 101, errMsg: "Content is required" }, 400);
+    } else if (!cleanAuthor) {
+      return c.json({ errCode: 102, errMsg: "Author is required" }, 400);
     }
 
-    const newPost = await PostModel.create(title, content, author);
+    const newPost = await PostModel.create(
+      cleanTitle,
+      cleanContent,
+      cleanAuthor,
+    );
     return c.json({ message: "Post created successfully", post: newPost }, 201);
   } catch (error) {
     console.error(error);
-    return c.json({ error: "Failed to create post" }, 500);
+    return c.json(
+      {
+        errCode: 104,
+        errMsg: "Failed to create post due to internal server error",
+      },
+      500,
+    );
   }
 };
 
