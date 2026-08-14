ad4c50bb148b7bafc2fd0fa372452f519d4416c6
Anindya <an1ndya@proton.me>
2026-08-14 13:29:15 +0600

    feat(backend): Add a REST API endpoint for post creation

---
+++ server/src/models/postModel.ts
@@ -0,0 +1,32 @@
+import { pool } from "../config/db.js";
+import type { Post } from "../types/postsType.js";
+
+export const PostModel = {
+  // CREATE
+  async create(title: string, content: string, author: string): Promise<Post> {
+    const query = `
+      INSERT INTO posts (title, content, author)
+      VALUES ($1, $2, $3)
+      RETURNING *
+    `;
+
+    const res = await pool.query(query, [title, content, author]);
+    return res.rows[0];
+  },
+
+  // READ
+  // async findAll(): Promise<Post[]> {
+  // },
+
+  // UPDATE
+  // async update(
+  //   id: number,
+  //   title: string,
+  //   content: string,
+  // ): Promise<Post | null> {
+  // },
+
+  // DELETE
+  // async delete(id: number): Promise<boolean> {
+  // },
+};
